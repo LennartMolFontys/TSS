c91d5a0d0cfc37f838d0168495a7d19f428e1b83
Lennart Mol <lennartmol@live.nl>
2019-03-14 15:11:55 +0100

    Connectie opgezet & Scan wagons
    Nog niet netjes.
    Delays moeten vervangen worden door millis.

--- Locomotief/Locomotief.ino
+++ Locomotief/Locomotief.ino
@@ -1,16 +1,69 @@
 #include <Wire.h>
+#include <SoftwareSerial.h>
+String addressString = "#1%";
+
+
+int staat = 0;
+int wagonNummer = 1;
+
+int wagon = 1;
+
+
+SoftwareSerial mySerial(5, 6); // RX, TX
 
 void setup() {
+  mySerial.begin(9600);
   Wire.begin();
   Serial.begin(9600);
 }
 
 void loop() {
-  Wire.requestFrom(8, 6);
+  mySerial.println(addressString);
+  switch (staat) {
+    case 0:
+      if (reactieVanWagon(wagonNummer)) {
+        Serial.print("Aantal wagons: ");
+        Serial.println(wagonNummer);
+        wagonNummer++;
+        staat = 0;
+      }
+      else {
+        wagonNummer--;
+        staat = 1;
+      }
+      break;
 
-  while (Wire.available()) {
-    char c = Wire.read();
-    Serial.print(c);
+    case 1:
+      requestSeats(wagon);
+      delay(500);
+      wagon++;
+      if (wagon > wagonNummer){
+        wagon = 1;
+      }
+      break;
   }
-  delay(500);
+}
+
+
+
+bool reactieVanWagon(int nummer) {
+  Wire.requestFrom(nummer, 1);
+  if (Wire.available()) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+void requestSeats(int f){
+  Wire.requestFrom(f, 2);
+      Serial.print("Wagon ");
+      Serial.print(f);
+      Serial.print(": ");
+      if (Wire.available()) {
+        int i = Wire.read() * 256 + Wire.read();
+        Serial.print(i);
+      }
+      Serial.println("");
 }
