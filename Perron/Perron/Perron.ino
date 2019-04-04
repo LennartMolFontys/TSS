1f84dfb4b8693a4f3d197d62e0afb67273c96d35
Pim van der Vliet <pimvliet@live.nl>
2019-04-04 13:16:00 +0200

    Perron werkend gemaakt met een seven segment display

--- Perron/Perron/Perron.ino
+++ Perron/Perron/Perron.ino
@@ -15,15 +15,17 @@
 #define LED3Red 10   // LED strip #1
 #define LED3Green 11
 
-#define readSerialState 0
+#define idle 0
 #define writeToDisplayState 1
 #define writeToLEDStripState 2
 
 /*------------------------------------------------==||## Variables ##||==------------------------------------------------*/
-int state = readSerialState;
-String message = "";
+int state = idle;
+String displayMessage = "";
+String lastDisplayMessage = "";
 
 /*------------------------------------------------==||## Setup ##||==------------------------------------------------*/
+TM1637Display display(CLK1, DIO1);
 
 void setup()
 {
@@ -35,20 +37,67 @@ void setup()
   pinMode(LED3Green, OUTPUT);
 
   Serial.begin(9600);
+  display.setBrightness(0x0f);
 }
 
 /*------------------------------------------------==||## Loop ##||==------------------------------------------------*/
 void loop()
 {
+  ReadSerial();
   switch (state)
   {
-    case readSerialState:
-      ReadSerial();
+    case idle:
+      if (displayMessage != lastDisplayMessage) {
+        state = writeToDisplayState;
+        Serial.println(displayMessage);
+      }
+      break;
+
+    case writeToDisplayState:
+      if (WriteToDisplay(displayMessage, lastDisplayMessage))
+      {
+        state = writeToLEDStripState;
+      }
+      break;
+
+    case writeToLEDStripState:
+      lastDisplayMessage = displayMessage;
+      Serial.println(lastDisplayMessage);
+      state = idle;
       break;
   }
 }
 
-String ReadSerial()
+/*------------------------------------------------==||## Read Serial ##||==------------------------------------------------*/
+void ReadSerial()
 {
-  
+  static String message = "";
+  if (Serial.available() > 0) {
+    char readChar = (char) Serial.read();
+    message = message + readChar;
+    message.trim();
+  }
+  if (message.startsWith("#") && message.endsWith("%")) {
+    message = message.substring(1, message.length() - 1);
+    displayMessage = message;
+    message = "";
+  }
+}
+
+/*------------------------------------------------==||## Write Message To Display ##||==------------------------------------------------*/
+bool WriteToDisplay(String messageToDisplay, String lastMessageToDisplay)
+{
+  static int displayNumber = 0;
+  int displayText = messageToDisplay.substring((displayNumber * 4), (displayNumber * 4) + 4).toInt();
+  display.showNumberDec(displayText, true);
+  if ((displayNumber + 1) >= ((messageToDisplay.length() + 1) / 4))
+  {
+    displayNumber = 0;
+    return true;
+  }
+  else
+  {
+    displayNumber++;
+    return false;
+  }
 }
