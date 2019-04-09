dcc65ba1760cc976ac4fcddf4761e038e57f07bb
BerendPijnenburg <47556984+BerendPijnenburg@users.noreply.github.com>
2019-04-09 09:40:30 +0200

    String splitter

---
+++ stringSplitter/stringSplitter/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace stringSplitter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string initialiseString = "ID:2365UnitAmount:3Length:3TotalSeats:30Length:5TotalSeats:30Length:6TotalSeats:30";
+            label1.Text = StringSplitter.GetTrainId(initialiseString).ToString();
+            StringSplitter.GetUnitInfo(initialiseString, StringSplitter.GetUnitAmount(initialiseString));
+        }
+    }
+}
---
+++ stringSplitter/stringSplitter/StringSplitter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringSplitter
+{
+    static class StringSplitter
+    {
+
+        public static int GetTrainId(string initialiseString)
+        {
+            string id = "ID:";
+            int startPoint = initialiseString.IndexOf(id) + id.Length;
+            int length = initialiseString.IndexOf("UnitAmount") - startPoint;
+            int trainId = Convert.ToInt32(initialiseString.Substring(startPoint, length));
+            return trainId;
+        }
+        
+        public static int GetUnitAmount(string initialiseString)
+        {
+            string unit = "UnitAmount:";
+            int startPoint = initialiseString.IndexOf(unit) + unit.Length;
+            int length = initialiseString.IndexOf("Length") - startPoint;
+            int unitAmount = Convert.ToInt32(initialiseString.Substring(startPoint, length));
+            return unitAmount;
+        }
+
+        public static List<int[]> GetUnitInfo(string initialiseString, int unitAmount)
+        {
+            string lengthString = "Length:";
+            string seatsString = "TotalSeats:";
+            int searchStartPoint = 0;
+            List<int[]> unitInfo = new List<int[]>();
+            for(int i = 0; i < unitAmount; i++)
+            {
+                int startPoint = initialiseString.IndexOf(lengthString, searchStartPoint) + lengthString.Length;
+                searchStartPoint = startPoint;
+                int length = initialiseString.IndexOf(seatsString, searchStartPoint) - startPoint;
+                int unitLength = Convert.ToInt32(initialiseString.Substring(startPoint, length));
+
+                startPoint = initialiseString.IndexOf(seatsString, searchStartPoint) + seatsString.Length;
+                searchStartPoint = startPoint;
+                length = initialiseString.IndexOf(lengthString, searchStartPoint) - startPoint;
+                if(length < 0)
+                {
+                    length = initialiseString.Length - startPoint;
+                }
+                int seatAmount = Convert.ToInt32(initialiseString.Substring(startPoint, length));
+                unitInfo.Add(new int[2] { unitLength, seatAmount });
+            }
+            return unitInfo;
+        }
+    }
+}
