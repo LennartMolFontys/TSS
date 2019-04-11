cddf710987d4afa6e0db8101492badb4e20d7bf7
Pim van der Vliet <pimvliet@live.nl>
2019-04-11 12:22:42 +0200

    Perron Applicatie Werkend Voor Demo

--- Perron/C# Code/Platform/Platform/Form1.cs
+++ Perron/C# Code/Platform/Platform/Form1.cs
@@ -15,15 +15,15 @@ namespace Platform
     {
         private PlatForm platForm = new PlatForm();
         private Display display = new Display(7);
-       // private Label[] labels = new Label[9];
+        private Label[] labels;
 
         public Form1()
         {
             InitializeComponent();
-           // labels = { Unit1LB, Unit2LB, Unit3Lb, Unit4Lb, Unit5Lb, Unit6Lb, Unit7Lb, Unit8Lb }
+           labels = new Label[]{ Unit1LB, Unit2LB, Unit3Lb, Unit4Lb, Unit5Lb, Unit6Lb, Unit7Lb, Unit8Lb };
             try
             {
-                platForm.Connect("145.93.61.189", 8888);
+                platForm.Connect("145.93.40.170", 8888);
                 display.Connect();
             }
             catch (InvalidOperationException Exception)
@@ -51,25 +51,24 @@ namespace Platform
         private void TreinInfoBtn_Click(object sender, EventArgs e)
         {
             platForm.GetTrainInfo();
-            platForm.GetSeatInfo();
             idLabel.Text = platForm.TrainID.ToString();
-            //LabelFiller();
-            display.Send(platForm.send());
 
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             platForm.GetSeatInfo();
+            LabelFiller();
+            display.Send(platForm.send());
         }
 
-        /* private void LabelFiller()
+        private void LabelFiller()
          {
              for(int i = 0; i < platForm.trainUnits; i++)
              {
                  labels[i].Text = platForm.freeSeats[i].ToString();
              }
-         }*/
+         }
 
     }
 }
