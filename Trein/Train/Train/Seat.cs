bddcc31ffced0a7c2eea4e93e3ee0396e9f623ab
jeffreyPieters <45628912+jeffreyPieters@users.noreply.github.com>
2019-04-25 13:41:17 +0200

    Seats Taken Working
    Tested and Created Seat Taken

---
+++ Trein/Train/Train/Seat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Train
+{
+     public class Seat
+    {
+        public int NrSeatsFirstClass { get; private set; }
+        public int NrSeatsSecondClass { get; private set; }
+        public bool Taken { get; private set; }
+
+        public Seat(bool taken)
+        {
+            Taken = taken;
+        }
+
+        public void SetTaken()
+        {
+            Taken = true;
+        }
+
+    }
+}
---
+++ Trein/Train/Train/TrainUnit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Train
+{
+    public abstract class TrainUnit
+    {
+        public int Lenght { get; private set; }
+
+        public List<string> SeatsTaken { get; private set; }
+
+        public int SeatsTotal { get; private set; }
+
+        public TrainUnit(int Lenght, int SeatsTotal)
+        {
+            SeatsTaken = new List<string>();
+            this.Lenght = Lenght;
+            this.SeatsTotal = SeatsTotal;
+        }
+
+        public abstract int GetFreeSeats();
+        public abstract void SetSeatTaken(int[] index, List<string> TakenSeats);
+
+    }
+}
