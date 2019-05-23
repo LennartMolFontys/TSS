be7917f536bba63b0baaa4031e61e6e6bfe934ed
BerendPijnenburg <47556984+BerendPijnenburg@users.noreply.github.com>
2019-05-23 14:54:46 +0200

    IDisplay interface added

---
+++ Perron/C# Code/Platform/Platform/IDisplay.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platform
+{
+    interface IDisplay
+    {
+        bool Connected { get;}
+        void Connect();
+
+        void Disconnect();
+
+        void Send(string info);
+    }
+}
